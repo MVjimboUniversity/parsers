c5012bb918d25dacbb94fe2787bd932fe79482be
dyushakolesov <dyushakolesov@mail.ru>
2023-03-12 23:25:47 +0300

    pdf test added

--- tests/test_pdf_parser.py
+++ tests/test_pdf_parser.py
@@ -1,19 +1,20 @@
 from parsers.pdf_parser import parser
 
-EXPECTED_TEXTS = ['http://info.cern.ch', 'http://info.cern.ch - home of the first website',
-                   'From here you can:', 'Browse the first website', 'Browse the first website using the line-mode browser simulator',
-                   'Learn about the birth of the web', 'Learn about CERN, the physics laboratory where the web was born']
-EXPECTED_URLS = ['http://info.cern.ch/hypertext/WWW/TheProject.html', 'http://line-mode.cern.ch/www/hypertext/WWW/TheProject.html', 'http://home.web.cern.ch/topics/birth-web', 'http://home.web.cern.ch/about']
 
-URL_local = r"test_files\multieng test.pdf"
+URL_local = r"test_files\Hello world test_advanced.pdf"
 URL_global = r'http://www-personal.umich.edu/~mejn/courses/2006/cmplxsys899/powerlaws.pdf'
+URL_multieng = r"test_files\multieng test.pdf"
+
+EXPECTED_TEXT = """HELLO WORLD!!! Advanced-technolocies’s in 2015` year./ \x0c"""
 
 
+def test_parser_correctness_multieng():
+    text = parser(URL_multieng)
+    assert  text[0] == 'Файл WORD был преобразован в PDF HELLO WORLD!!! \x0c', f'texts do not match, {text[0]} expected'
 
 def test_parser_correctness():
     text = parser(URL_local)
-    assert  text[0] == 'Файл WORD был преобразован в PDF HELLO WORLD!!! \x0c', f'texts do not match, {text[0]} expected'
-
+    assert  text[0] == EXPECTED_TEXT, f'texts do not match, {text[0]} expected'
 
 def test_parser_page_counts():
     text = parser(URL_global)
